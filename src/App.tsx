89b62e08ab74c813a2a86cfd74017fdf8f95abcd
Dávid Kecskeméti <dxdy421@gmail.com>
2021-02-04 00:31:02 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useQuery } from "react-query";
 
-function App() {
+//styles
+import "./App.scss";
+
+type BitcoinData = {
+  "15m": number;
+  buy: number;
+  last: number;
+  sell: number;
+  symbol: string;
+};
+
+type Currencies = {
+  [key: string]: BitcoinData;
+};
+
+const getBCData = async (): Promise<Currencies> =>
+  await (await fetch("https://www.blockchain.com/ticker")).json();
+
+const INTERVAL_TIME = 10000; //30s
+
+const App = () => {
+  const [currency, setCurrency] = useState("USD");
+  const { data, isLoading, error, refetch } = useQuery<Currencies>(
+    "bc-data",
+    getBCData
+  );
+  console.log(data);
+
+  useEffect(() => {
+    const interval = setInterval(refetch, INTERVAL_TIME);
+    return () => clearInterval(interval);
+  }, [refetch]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Something went wrong...</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="wrapper">
+      <>
+        <h2>Bitcoint price</h2>
+        <select
+          value={currency}
+          onChange={(e: any) => setCurrency(e.currentTarget.value)}
         >
-          Learn React
-        </a>
-      </header>
+          {data &&
+            Object.keys(data).map((currency) => (
+              <option key={currency}>{currency}</option>
+            ))}
+        </select>
+        <div>
+          <h2>
+            {data && data[currency].symbol}
+            {data && data[currency].last}
+          </h2>
+        </div>
+      </>
     </div>
   );
-}
+};
 
 export default App;
